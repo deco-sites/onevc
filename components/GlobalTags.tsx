d6aa623caba24ae9f57c13ce38ae36697d559b0e
gustavo-vasconcellos <gustavo.vasconcellos@hp.com>
2023-04-11 19:35:52 -0300

    feat: add hero animations

--- components/GlobalTags.tsx
+++ components/GlobalTags.tsx
@@ -21,6 +21,16 @@ function GlobalTags() {
         type="image/x-icon"
         href={asset("/favicon-32x32.png")}
       />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="true"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet"
+      />
 
       {/* Web Manifest */}
       <link rel="manifest" href={asset("/site.webmanifest")} />
