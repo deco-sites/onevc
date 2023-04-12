9f4534de582250e77005953c1738b4695b6f5ac8
gustavo-vasconcellos <gustavo.vasconcellos@hp.com>
2023-04-12 19:29:23 -0300

    style: auto format files

--- components/hero/HeroPicture.tsx
+++ components/hero/HeroPicture.tsx
@@ -33,7 +33,9 @@ function HeroPicture({ image, imageAlt }: Props) {
   return (
     <div
       class={`${
-        hideImage.value ? "translate-y-[-50%] md:(translate-x-[30px]) scale-[0.6] opacity-0" : ""
+        hideImage.value
+          ? "translate-y-[-50%] md:(translate-x-[30px]) scale-[0.6] opacity-0"
+          : ""
       } w-full max-w-[60vw] block relative left-auto top-auto transition ease-in-out duration-[.6s] pb-[50px] md:(fixed translate-y-[-50%] max-w-[501px] left-[50%] top-[50%] pb-[100px])`}
     >
       <div class="animate-hero-image">
