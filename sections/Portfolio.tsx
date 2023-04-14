f5ad54b8c10d5c05c80aa722266b501fe58c8f7f
gustavo-vasconcellos <gustavo.vasconcellos@hp.com>
2023-04-14 19:50:08 -0300

    style: format files

--- sections/Portfolio.tsx
+++ sections/Portfolio.tsx
@@ -1,10 +1,10 @@
 export { default } from "deco-sites/onevc/components/portfolio/Portfolio.tsx";
 export type {
   Detail,
+  Item,
   ItemContent,
   ItemImage,
   Link,
-  Item,
   Section,
 } from "deco-sites/onevc/components/portfolio/types.ts";
 export type { Image as LiveImage } from "deco-sites/std/components/types.ts";
