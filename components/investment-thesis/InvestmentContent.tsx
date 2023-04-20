5c7a3ce5c01a9618a07dfd19df652bacb633d6ce
gustavo-vasconcellos <gustavo.vasconcellos@hp.com>
2023-04-20 17:16:32 -0300

    feat: add investment content animation

--- components/investment-thesis/InvestmentContent.tsx
+++ components/investment-thesis/InvestmentContent.tsx
@@ -1,5 +1,11 @@
 import type { Item } from "deco-sites/onevc/components/investment-thesis/InvestmentThesis.tsx";
-import InvestmentUtils from "deco-sites/onevc/islands/InvestmentUtils.tsx";
+import InvestmentUtils from "deco-sites/onevc/components/investment-thesis/InvestmentUtils.tsx";
+import { replaceBreakLines } from "deco-sites/onevc/sdk/format.tsx";
+import { useUI } from "deco-sites/onevc/sdk/useUI.ts";
+import { useSignal } from "@preact/signals";
+import { useEffect, useRef } from "preact/compat";
+import { tw } from "twind/css";
+import { IS_BROWSER } from "$fresh/runtime.ts";
 
 export interface Props {
   text: string;
@@ -7,12 +13,58 @@ export interface Props {
 }
 
 export function Content({ text, items }: Props) {
+  const { displayTab } = useUI();
+  const visible = useSignal(false);
+  const ref = useRef<HTMLUListElement>(null);
+
+  useEffect(() => {
+    if (!ref.current || !IS_BROWSER) return;
+
+    const intersectionObserver = new IntersectionObserver(
+      (entries: IntersectionObserverEntry[]) => {
+        visible.value = entries[0].isIntersecting;
+
+        if (entries[0].isIntersecting) {
+          intersectionObserver.disconnect();
+        }
+      },
+      { root: null, rootMargin: "0px", threshold: 1.0 },
+    );
+
+    intersectionObserver.observe(ref.current);
+    return () => intersectionObserver.disconnect();
+  }, [ref.current]);
+
+  const transition = tw(() => ({
+    "-webkit-transition":
+      "opacity .5s ease-out, left .5s ease-out, top .5s ease-out",
+    "transition": "opacity .5s ease-out, left .5s ease-out, top .5s ease-out",
+    "transition-delay": "500ms",
+  }));
+
+  const showElements = (direction: string) =>
+    tw`override:(opacity-100 ${direction}-[0px])`;
+  const hideElements = (direction: string) =>
+    tw`override:(opacity-0 ${direction}-[50px])`;
+
+  const commonClasses = tw`${transition} relative opacity-0`;
+
+  const textClasses = tw`${
+    visible.value && displayTab.value === null
+      ? showElements("left")
+      : hideElements("left")
+  } ${commonClasses} text-[30px] font-bold leading-[37px]`;
+
+  const listClasses = tw`${
+    visible.value ? showElements("top") : hideElements("top")
+  } ${commonClasses}`;
+
   return (
     <>
-      <p class="c-investment__text">
-        {text}
-      </p>
-      <ul class="c-investment-list">
+      <div class={textClasses}>
+        {replaceBreakLines(text)}
+      </div>
+      <ul class={listClasses} ref={ref}>
         {items.map((item, index) => (
           <InvestmentUtils type="item" item={item} index={index} key={index} />
         ))}
